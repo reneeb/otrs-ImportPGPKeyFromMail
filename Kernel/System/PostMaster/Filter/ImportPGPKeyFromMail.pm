bdf9c73176aaed4bbf273466f96bb0b131740527
reneeb <info@perl-services.de>
2017-07-13 12:09:57 +0200

    handle encrypted mails as well

--- Kernel/System/PostMaster/Filter/ImportPGPKeyFromMail.pm
+++ Kernel/System/PostMaster/Filter/ImportPGPKeyFromMail.pm
@@ -55,14 +55,46 @@ sub Run {
 
     my %Mail = %{ $Param{GetParam} || {} };
 
+    # Try to get message & encryption method.
+    my $Message;
+    my $ContentType;
+    my $EncryptionMethod = '';
+
+    if ( $Mail{Body} =~ /\A[\s\n]*^-----BEGIN PGP MESSAGE-----/m ) {
+        $Message          = $Mail{Body};
+        $ContentType      = $Mail{'Content-Type'} || '';
+        $EncryptionMethod = 'PGP';
+    }
+    else {
+        CONTENT:
+        for my $Content ( @{ $Mail{Attachment} } ) {
+            if ( $Content->{Content} =~ /\A[\s\n]*^-----BEGIN PGP MESSAGE-----/m ) {
+                $Message          = $Content->{Content};
+                $ContentType      = $Content->{ContentType} || '';
+                $EncryptionMethod = 'PGP';
+                last CONTENT;
+            }
+        }
+    }
+
+    if ( $EncryptionMethod eq 'PGP' && $ContentType =~ m{application/(?: pgp | octet-stream )}xsm ) {
+
+        my $CryptObject = $Kernel::OM->Get('Kernel::System::Crypt::PGP');
+
+        my %Decrypt = $CryptObject->Decrypt( Message => $Message );
+
+        return if !$Decrypt{Successful};
+
+        my $ParserObject = Kernel::System::EmailParser->new( %{$Self}, Email => $Decrypt{Data} );
+
+        $Mail{Attachment} = [ $ParserObject->GetAttachments() ];
+    }
+
     return 1 if !$Mail{Attachment};
 
     my @KeyFiles = grep{ $_->{Filename} =~ m{\.asc \z}ismx }@{ $Mail{Attachment} };
 
     return 1 if !@KeyFiles;
-
-    $LogObject->Log( Priority => error => Message => $Kernel::OM->Get('Kernel::System::Main')->Dump( \@KeyFiles ) );
-    $LogObject->Log( Priority => error => Message => $Kernel::OM->Get('Kernel::System::Main')->Dump( $Mail{Attachment} ) );
 
     for my $KeyFile ( @KeyFiles ) {
         $PGPObject->KeyAdd(
